33f9937322313dd8074ff73756d18a35d7f648b1
Abdogouhmad <gouhmada@gmail.com>
2024-09-01 16:48:41 +0100

    the rcn is ready to use

---
+++ README.md
@@ -0,0 +1,63 @@
+# RCN - React Component Generator
+
+RCN is a command-line tool designed to generate React components with the necessary files. It supports both JavaScript and TypeScript components.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Commands](#commands)
+- [Examples](#examples)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+To use RCN, you need to have Rust installed on your system. You can download and install Rust from [rust-lang.org](https://www.rust-lang.org/).
+
+Once Rust is installed, you can clone this repository and build the tool using Cargo:
+
+```sh
+cargo install https://github.com/Abdogouhmad/rcn
+```
+
+## Usage
+
+```
+rcn <Command> [options]
+```
+
+## Commands
+### modeles
+
+Generates a React component with the specified name and language (JavaScript or TypeScript).
+```bash
+rcn modeles <name> <language>
+
+    <name>: The name of the component.
+    <language>: The language of the component (js for JavaScript, ts for TypeScript).
+```
+
+### help
+
+Displays the help message with information about the available commands and their usage.
+```bash
+rcn help
+# or 
+rcn -h
+```
+
+### Examples
+Generate a JavaScript or TS React Component
+
+```
+rcn modeles MyComponent js/ts
+```
+This command will create a directory components/MyComponent with the following files:
+
+````
+- MyComponent.component.jsx/tsx
+- MyComponent.service.js/ts
+- MyComponent.types.ts # js won't generate the file types.js 
+````
+
---
+++ src/main.rs
@@ -0,0 +1,117 @@
+use std::{env, fs, io::Error, path::Path};
+
+struct Rcn {
+    subcommands: Option<Commands>,
+}
+
+enum Commands {
+    Modeles { name: String, ts_js: String },
+    Help,
+}
+
+impl Rcn {
+    pub fn parse() -> Self {
+        let args: Vec<String> = env::args().collect();
+
+        if args.len() < 2 {
+            return Self { subcommands: None };
+        }
+
+        let subcommand = &args[1];
+
+        match subcommand.as_str() {
+            "modeles" => {
+                Self::args_check("modeles".to_string(), &args);
+                let name = args[2].clone();
+                let ts_js = args[3].clone();
+                Self {
+                    subcommands: Some(Commands::Modeles { name, ts_js }),
+                }
+            }
+            "help" | "-h" => Self {
+                subcommands: Some(Commands::Help),
+            },
+            _ => {
+                eprintln!("Error: Unknown subcommand '{}'.", subcommand);
+                std::process::exit(1);
+            }
+        }
+    }
+
+    // check the arguments
+    fn args_check(commandname: String, args: &[String]) {
+        if args.len() < 3 {
+            eprintln!(
+                "Error: '{}' subcommand requires a name argument.",
+                commandname
+            );
+            std::process::exit(1);
+        }
+    }
+
+    // Print the help message
+    pub fn handle_help() {
+        println!(
+            "rcn is a react component generator that generates the component with needed files\n"
+        );
+        println!("Usage: rcn <subcommand> [options]");
+        println!("\n");
+        println!("Subcommands:");
+        println!("  modeles <name> <language>    Generate a React component with the specified name and language (js or ts).");
+        println!("  help                         Display this help message.");
+        println!("\n");
+        println!("Examples:");
+        println!("  rcn modeles MyComponent js   Generate a JavaScript React component named MyComponent.");
+        println!("  rcn modeles MyComponent ts   Generate a TypeScript React component named MyComponent.");
+        println!("  rcn help/-h                     Display this help message.");
+    }
+}
+
+// Implementation for Commands (if needed)
+impl Commands {
+    pub fn create_modele(name: String, ts_js: String) -> Result<(), Error> {
+        let extensions_js = vec![".component.jsx", ".service.js"];
+        let extensions_ts = vec![".component.tsx", ".service.ts", ".types.ts"];
+
+        let dir_compo = format!("components/{}", name);
+
+        // Create the directory if it doesn't exist
+        if !Path::new(&dir_compo).exists() {
+            fs::create_dir_all(&dir_compo)?;
+        }
+
+        let extensions = if ts_js.contains("js") {
+            extensions_js
+        } else if ts_js.contains("ts") {
+            extensions_ts
+        } else {
+            eprintln!("Unsupported language: {}", ts_js);
+            std::process::exit(1);
+        };
+
+        for extension in extensions {
+            let file_to_create = format!("{}{}", name, extension);
+            let file_path = Path::new(&dir_compo).join(&file_to_create);
+
+            // Create the file
+            fs::File::create(file_path.clone())?;
+
+            println!("Created file: {}", file_path.display());
+        }
+        Ok(())
+    }
+}
+
+fn main() {
+    let rcn = Rcn::parse();
+
+    match rcn.subcommands {
+        Some(Commands::Modeles { name, ts_js }) => {
+            Commands::create_modele(name, ts_js).expect("can't create");
+        }
+        Some(Commands::Help) => Rcn::handle_help(),
+        None => {
+            println!("No subcommand provided.");
+        }
+    }
+}
